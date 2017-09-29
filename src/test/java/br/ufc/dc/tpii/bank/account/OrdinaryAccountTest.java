6f0be7ea9b11b11b888e459b8dcacf292ad0fd0f
thiagoripardo <tmripardo@gmail.com>
2017-09-29 09:26:20 -0300

    Configurando testes com maven

---
+++ src/test/java/br/ufc/dc/tpii/bank/account/OrdinaryAccountTest.java
@@ -0,0 +1,80 @@
+package br.ufc.dc.tpii.bank.account;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ufc.dc.tpii.bank.account.OrdinaryAccount;
+import br.ufc.dc.tpii.bank.account.exception.InsufficientFundsException;
+import br.ufc.dc.tpii.bank.account.exception.NegativeAmountException;
+
+public class OrdinaryAccountTest {
+
+	private OrdinaryAccount account;
+	
+	@Before
+	public void setUp() throws Exception {
+		account = new OrdinaryAccount("123A");
+	}
+	
+	@Test
+	public void testCredit50AndDebit30(){
+		try {
+			account.credit(50);
+			assertEquals("Erro na op", 50, account.getBalance(), 0.0);
+			account.debit(30);
+			assertEquals("Erro na op", 20, account.getBalance(), 0.0);
+		} catch (NegativeAmountException e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		} catch (InsufficientFundsException e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testDebit() {
+		/*try {
+			account.debit(30);
+			assertEquals("message", 30, account.getBalance(), 0.0);
+		} catch (NegativeAmountException e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		} catch (InsufficientFundsException e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		}*/
+	}
+
+	@Test
+	public void testOrdinaryAccount() {
+		/*
+		try {
+			account.debit(30);
+			assertEquals("message", 30, account.getBalance(), 0.0);
+		} catch (NegativeAmountException e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		} catch (InsufficientFundsException e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		}*/
+	}
+
+	@Test
+	public void testCredit() {
+		/*
+		try {
+			account.debit(30);
+			assertEquals("message", 30, account.getBalance(), 0.0);
+		} catch (NegativeAmountException e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		} catch (InsufficientFundsException e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		}*/
+	}
+
+}
